cc7ff51a315f8116381c70fb38e9eb2abbb7ed91
Maksym Koval <maxonchil@gmail.com>
2024-03-19 14:49:15 +0200

    Added translations

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,36 @@
-import { enableProdMode } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
+export const httpLoaderFactory = (http: HttpClient): TranslateLoader =>
+  new TranslateHttpLoader(http, './assets/i18n/', '.json');
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        defaultLanguage: 'ua',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: httpLoaderFactory,
+          deps: [HttpClient],
+        },
+      }),
+    ),
   ],
 });
+
+if (environment.production) {
+  enableProdMode();
+}
