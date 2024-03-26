d88bb68ec91330355a52bf6487d16b284eafbac5
Maksym Koval <maxonchil@gmail.com>
2024-03-26 21:04:00 +0200

    Finished registration page basic implementation
    * Added layout for form;
    * Added basic validation and erorrs displaying;
    * Added new translation keys for form;
    * Covered new functionality with unit tests;

--- src/app/pages/public/registration/registration.page.spec.ts
+++ src/app/pages/public/registration/registration.page.spec.ts
@@ -1,24 +1,66 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { NonNullableFormBuilder } from '@angular/forms';
+import { classWithProviders } from '@ngx-unit-test/inject-mocks';
+import { MockProxy, mock } from 'jest-mock-extended';
 
-import { RegistrationComponent } from './registration.component';
+import RegistrationPageComponent from './registration.page';
 
 describe('RegistrationComponent', () => {
-  let component: RegistrationComponent;
-  let fixture: ComponentFixture<RegistrationComponent>;
+  let component: RegistrationPageComponent;
+  let formBuilderMock: MockProxy<NonNullableFormBuilder>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ RegistrationComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(() => {
+    formBuilderMock = mock<NonNullableFormBuilder>();
 
-    fixture = TestBed.createComponent(RegistrationComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+    component = classWithProviders({
+      token: RegistrationPageComponent,
+      providers: [
+        {
+          provide: NonNullableFormBuilder,
+          useValue: formBuilderMock,
+        },
+      ],
+    });
+  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('togglePasswordReveal', () => {
+    it('should trigger "set" method of signal with oposite value', () => {
+      const spy = jest.spyOn(component.isPasswordRevealed, 'set');
+
+      component.togglePasswordReveal();
+
+      expect(spy).toHaveBeenCalledWith(true);
+    });
+  });
+
+  describe('toggleConfirmPasswordReveal', () => {
+    it('should trigger "set" method of signal with oposite value', () => {
+      const spy = jest.spyOn(component.isConfirmPasswordRevealed, 'set');
+
+      component.toggleConfirmPasswordReveal();
+
+      expect(spy).toHaveBeenCalledWith(true);
+    });
+  });
+
+  describe('getEmailError', () => {
+    it('should return "validation.required" text if such error in form control', () => {
+      const result = component.getEmailError({ required: true });
+
+      expect(result).toBe('validation.required');
+    });
+
+    it('should return "validation.invalid_email" text if control error is not required', () => {
+      const result = component.getEmailError({ noRequired: true });
+
+      expect(result).toBe('validation.invalid_email');
+    });
+  });
+
+  describe('getConfirmPasswordError', () => {
+    it('should return "validation.passwords_not_match" error if password does not match', () => {
+      const result = component.getConfirmPasswordError({ notMatchedPassword: true }, {});
+
+      expect(result).toBe('validation.passwords_not_match');
+    });
   });
 });
--- src/app/pages/public/registration/registration.page.ts
+++ src/app/pages/public/registration/registration.page.ts
@@ -1,7 +1,11 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { FormControl, NonNullableFormBuilder, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
+import { addIcons } from 'ionicons';
+import { eyeOffOutline, eyeOutline } from 'ionicons/icons';
 import { EMAIL_REGEXP } from 'src/app/constants/regular-expressions';
 
 import { RegistrationForm } from './registration.page.models';
@@ -10,12 +14,15 @@ import { RegistrationValidators } from './registration.page.validators';
 @Component({
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [IonicModule, ReactiveFormsModule, TranslateModule],
+  imports: [IonicModule, ReactiveFormsModule, TranslateModule, RouterLink, AsyncPipe],
   templateUrl: './registration.page.html',
   styleUrls: ['./registration.page.scss'],
 })
-export default class RegistrationComponent {
+export default class RegistrationPageComponent {
   readonly formBuilder = inject(NonNullableFormBuilder);
+
+  isPasswordRevealed = signal(false);
+  isConfirmPasswordRevealed = signal(false);
 
   registrationForm = this.formBuilder.group<RegistrationForm>(
     {
@@ -26,6 +33,43 @@ export default class RegistrationComponent {
     },
     { validators: RegistrationValidators.passwordsMatch },
   );
+
+  get emailControl(): FormControl<string | undefined> {
+    return <FormControl<string | undefined>>this.registrationForm.get('email');
+  }
+
+  get passwordControl(): FormControl<string | undefined> {
+    return <FormControl<string | undefined>>this.registrationForm.get('password');
+  }
+
+  get confirmPasswordControl(): FormControl<string | undefined> {
+    return <FormControl<string | undefined>>this.registrationForm.get('confirmPassword');
+  }
+
+  constructor() {
+    addIcons({ eyeOutline, eyeOffOutline });
+  }
+
+  togglePasswordReveal(): void {
+    this.isPasswordRevealed.set(!this.isPasswordRevealed());
+  }
+
+  toggleConfirmPasswordReveal(): void {
+    this.isConfirmPasswordRevealed.set(!this.isConfirmPasswordRevealed());
+  }
+
+  getEmailError(errors: ValidationErrors | null): string {
+    return errors?.['required'] ? 'validation.required' : 'validation.invalid_email';
+  }
+
+  getPasswordError(errors: ValidationErrors | null): string {
+    return 'validation.required';
+  }
+
+  getConfirmPasswordError(formErrors: ValidationErrors | null, controlErros: ValidationErrors | null): string {
+    if (formErrors?.['notMatchedPassword']) return 'validation.passwords_not_match';
+    return 'validation.required';
+  }
 
   onRegistrationFormSubmit(): void {
     console.log('submit');
