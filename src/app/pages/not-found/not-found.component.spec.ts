e16fe8fc7aed738e1a4ab891a43ccd604460ad21
Maksym Koval <maxonchil@gmail.com>
2024-04-20 17:54:31 +0300

    * Added not found page;

---
+++ src/app/pages/not-found/not-found.component.spec.ts
@@ -0,0 +1,13 @@
+import NotFoundComponent from './not-found.component';
+
+describe('NotFoundComponent', () => {
+  let component: NotFoundComponent;
+
+  beforeEach(() => {
+    component = new NotFoundComponent();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
