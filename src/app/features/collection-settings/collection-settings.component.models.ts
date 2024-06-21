12e283b9e0a4793456c0e3412ef72af2b4be9241
Maksym Koval <maxonchil@gmail.com>
2024-06-21 17:51:19 +0300

    * Started working on layout for collection settings;
    * Added basic layout for collection progress settings;
    * Added enums for cards display mode and collection progres appearance;

---
+++ src/app/features/collection-settings/collection-settings.component.models.ts
@@ -0,0 +1,10 @@
+export enum CollectionProgressMode {
+  Numbers = 'numbers',
+  Percentages = 'percentages',
+  None = 'none',
+}
+
+export enum CardsDisplayMode {
+  Image = 'image',
+  Chip = 'chip',
+}
--- src/app/features/collection-settings/components/collection-settings/collection-settings.component.ts
+++ src/app/features/collection-settings/components/collection-settings/collection-settings.component.ts
@@ -1,5 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {
+  CardsDisplayMode,
+  CollectionProgressMode,
+} from '@features/collection-settings/collection-settings.component.models';
+import {
   IonButton,
   IonButtons,
   IonContent,
@@ -8,6 +12,12 @@ import {
   IonMenuToggle,
   IonTitle,
   IonToolbar,
+  IonSelect,
+  IonSelectOption,
+  IonList,
+  IonItem,
+  IonText,
+  IonTabButton,
 } from '@ionic/angular/standalone';
 import { TranslateModule } from '@ngx-translate/core';
 import { addIcons } from 'ionicons';
@@ -17,6 +27,10 @@ import { closeOutline } from 'ionicons/icons';
   selector: 'app-collection-settings',
   standalone: true,
   imports: [
+    IonTabButton,
+    IonText,
+    IonItem,
+    IonList,
     IonIcon,
     IonButton,
     IonButtons,
@@ -26,12 +40,20 @@ import { closeOutline } from 'ionicons/icons';
     IonContent,
     IonMenuToggle,
     TranslateModule,
+    IonSelect,
+    IonSelectOption,
   ],
   templateUrl: './collection-settings.component.html',
   styleUrl: './collection-settings.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CollectionSettingsComponent {
+  CollectionProgressMode = CollectionProgressMode;
+  settings = {
+    collectionProgressMode: CollectionProgressMode.Numbers,
+    rarityProgressMode: CollectionProgressMode.Percentages,
+    cardsDisplayMode: CardsDisplayMode.Chip,
+  };
   constructor() {
     addIcons({ closeOutline });
   }
