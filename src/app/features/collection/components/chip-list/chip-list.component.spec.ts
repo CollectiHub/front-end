8c6a265d28ef062c814cf137214387d82b9608d0
Maksym Voloshyn <mv.webdew@gmail.com>
2024-07-14 13:15:56 +0300

    feat: add toaster display for non-existent cards

--- src/app/features/collection/components/chip-list/chip-list.component.spec.ts
+++ src/app/features/collection/components/chip-list/chip-list.component.spec.ts
@@ -1,14 +1,37 @@
+import { Card, StatusCard } from '@models/collection.models';
+import { TranslateService } from '@ngx-translate/core';
 import { classWithProviders } from '@ngx-unit-test/inject-mocks';
+import { ToastColor } from '@services/toast/toast.models';
+import { ToastService } from '@services/toast/toast.service';
+import { MockProxy, mock } from 'jest-mock-extended';
+import { of } from 'rxjs';
 
 import { ChipListComponent } from './chip-list.component';
 
 describe(ChipListComponent.name, () => {
   let component: ChipListComponent;
+  let translateServiceMock: MockProxy<TranslateService>;
+  let toastServiceMock: MockProxy<ToastService>;
 
   beforeEach(() => {
+    translateServiceMock = mock<TranslateService>();
+    translateServiceMock.instant.mockImplementation(a => a);
+
+    toastServiceMock = mock<ToastService>();
+    toastServiceMock.open$.mockReturnValue(of({} as HTMLIonToastElement));
+
     component = classWithProviders({
       token: ChipListComponent,
-      providers: [],
+      providers: [
+        {
+          provide: TranslateService,
+          useValue: translateServiceMock,
+        },
+        {
+          provide: ToastService,
+          useValue: toastServiceMock,
+        },
+      ],
     });
   });
 
@@ -27,6 +50,38 @@ describe(ChipListComponent.name, () => {
       const isLoading = component.getLoadingStatus(['R-005'], chipId);
 
       expect(isLoading).toBe(false);
+    });
+  });
+
+  describe('handleChipClick', () => {
+    it('should translate message for toaster if the chip does not exist', () => {
+      const chip: Card = {
+        id: 'id-002',
+        status: StatusCard.NotExisting,
+        rarity: 'R',
+        serial_number: 'R-002',
+        character_name: 'FF',
+        image_url: 'assets/img/card-stub.jpg',
+      };
+
+      component.handleChipClick('id-002', chip);
+
+      expect(translateServiceMock.instant).toHaveBeenCalledWith('collection.card_not_exists.toast');
+    });
+
+    it('should open toast if the chip does not exist', () => {
+      const chip: Card = {
+        id: 'id-002',
+        status: StatusCard.NotExisting,
+        rarity: 'R',
+        serial_number: 'R-002',
+        character_name: 'FF',
+        image_url: 'assets/img/card-stub.jpg',
+      };
+
+      component.handleChipClick('id-002', chip);
+
+      expect(toastServiceMock.open$).toHaveBeenCalledWith('collection.card_not_exists.toast', ToastColor.Medium);
     });
   });
 });
--- src/app/features/collection/components/chip-list/chip-list.component.ts
+++ src/app/features/collection/components/chip-list/chip-list.component.ts
@@ -1,6 +1,10 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { ChipComponent } from '@components/chip/chip.component';
-import { Card } from '@models/collection.models';
+import { Card, StatusCard } from '@models/collection.models';
+import { TranslateService } from '@ngx-translate/core';
+import { ToastColor } from '@services/toast/toast.models';
+import { ToastService } from '@services/toast/toast.service';
+import { take } from 'rxjs';
 
 import { stubChipList } from './chip-list.stub';
 
@@ -13,6 +17,9 @@ import { stubChipList } from './chip-list.stub';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChipListComponent {
+  private readonly translateService = inject(TranslateService);
+  private readonly toastService = inject(ToastService);
+
   chipList = signal<Card[]>(stubChipList);
   chipsPendingResponse = signal<string[]>([]);
 
@@ -20,7 +27,14 @@ export class ChipListComponent {
     return chipIds.includes(chipId);
   }
 
-  handleChipClick(chipId: string): void {
+  handleChipClick(chipId: string, chip: Card): void {
+    if (chip.status === StatusCard.NotExisting) {
+      const message = this.translateService.instant('collection.card_not_exists.toast');
+      this.toastService.open$(message, ToastColor.Medium).pipe(take(1)).subscribe();
+
+      return;
+    }
+
     // TO DO
     console.log(chipId);
   }
